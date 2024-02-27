9a96b6557b0977b88a70a9724aa8e58030c0e3dd
theflyer1983 <theflyer1983@gmail.com>
2024-02-27 19:22:02 +0000

    Astro Islands with a Vue component

---
+++ src/components/Greeting.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  messages: string[];
+}>();
+
+const randomMessage = () =>
+  props.messages[Math.floor(Math.random() * props.messages.length)];
+
+function setGreeting() {
+  greeting.value = randomMessage();
+  console.log(greeting.value);
+}
+const greeting = ref(props.messages[0]);
+</script>
+
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting.</h3>
+    <button @click="setGreeting" class="border">New Greeting</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
